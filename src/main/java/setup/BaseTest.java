bb1127e1c41bc057d96573a30f96e75e1e635e21
OlgaPoddubnaia <catdance@mail.ru>
2021-02-11 19:25:34 +0300

    building hw3

--- src/main/java/setup/BaseTest.java
+++ src/main/java/setup/BaseTest.java
@@ -24,56 +24,6 @@ public class BaseTest extends ConfProperties implements IDriver {
         return po;
     }
 
-  /*  @Parameters({"platformName", "appType", "deviceName", "udid", "browserName", "app", "appPackage", "appActivity", "bundleId"})
-    @BeforeSuite(alwaysRun = true)
-    public void setUp(String platformName,
-                      String appType,
-                      @Optional("") String deviceName,
-                      @Optional("") String udid,
-                      @Optional("") String browserName,
-                      @Optional("") String app,
-                      @Optional("") String appPackage,
-                      @Optional("") String appActivity,
-                      @Optional("") String bundleId
-    ) throws Exception {
-        System.out.println("Before: app type - " + appType);
-        setAppiumDriver(platformName, deviceName, udid, browserName, app, appPackage, appActivity, bundleId);
-        setPageObject(appType, appiumDriver);
-
-    }*/
-
-       /* private void setAppiumDriver(String platformName, String deviceName, String udid,
-                                 String browserName, String app, String appPackage,
-                                 String appActivity, String bundleId) {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        //mandatory Android capabilities
-        capabilities.setCapability("platformName", platformName);
-        capabilities.setCapability("deviceName", deviceName);
-        capabilities.setCapability("udid", udid);
-
-        if (app.endsWith(".apk")) capabilities.setCapability("app", (new File(app)).getAbsolutePath());
-
-        capabilities.setCapability("browserName", browserName);
-        capabilities.setCapability("chromedriverDisableBuildCheck", "true");
-
-        //Capabilities for test of Android native app on EPAM Mobile Cloud
-        capabilities.setCapability("appPackage", appPackage);
-        capabilities.setCapability("appActivity", appActivity);
-
-        //Capabilities for test of iOS native app on EPAM Mobile Cloud
-        capabilities.setCapability("bundleId", bundleId);
-
-        try {
-            appiumDriver = new AppiumDriver(new URL(System.getProperty("ts.appium")), capabilities);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-
-        // Timeouts tuning
-        appiumDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-    }*/
-
 
     @Parameters({"platformName", "appType", "deviceName", "udid", "browserName", "app", "appPackage", "appActivity", "bundleId"})
     @BeforeSuite(alwaysRun = true)
