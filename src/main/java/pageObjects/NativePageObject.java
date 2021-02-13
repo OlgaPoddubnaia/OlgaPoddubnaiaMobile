7f84cd67a54cf3408e19751134fe1220f11d9523
OlgaPoddubnaia <catdance@mail.ru>
2021-02-13 18:35:57 +0300

    создала часть локаторов для iOS

--- src/main/java/pageObjects/NativePageObject.java
+++ src/main/java/pageObjects/NativePageObject.java
@@ -10,54 +10,55 @@ import org.openqa.selenium.support.PageFactory;
 public class NativePageObject {
 
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/email_sign_in_button")
-    @iOSXCUITFindBy()
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeStaticText[@label='Sign in']")
     WebElement signInBtn;
 
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/register_button")
-    @iOSXCUITFindBy()
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeStaticText[@label='Register new account']")
     WebElement registerBtn;
 
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/login_email")
-    @iOSXCUITFindBy()
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeTextField")
     WebElement loginEmail;
 
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/login_pwd")
-    @iOSXCUITFindBy()
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeSecureTextField")
     WebElement loginPassword;
 
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_email")
-    @iOSXCUITFindBy()
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeTextField[@label='Email']")
     WebElement email;
 
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_username")
-    @iOSXCUITFindBy()
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeTextField[@label='Username']")
     WebElement username;
 
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_password")
-    @iOSXCUITFindBy()
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeSecureTextField@label='Password']")
     WebElement password;
 
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/registration_confirm_password")
-    @iOSXCUITFindBy()
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeSecureTextField@label='Confirm password']")
     WebElement confirmPassword;
 
     @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget." +
             "FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.LinearLayout/android." +
             "widget.ScrollView/android.widget.LinearLayout/android.widget.CheckedTextView")
-    @iOSXCUITFindBy()
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeSwitch")
     WebElement agreeCheckbox;
 
     @AndroidFindBy(id = "platkovsky.alexey.epamtestapp:id/register_new_account_button")
-    @iOSXCUITFindBy()
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeStaticText[@label='Register new account']")
     WebElement registerNewAccBtn;
 
     @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget." +
             "FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.view.ViewGroup/android" +
             ".widget.TextView")
-    @iOSXCUITFindBy()
+    // @iOSXCUITFindBy()
     WebElement budgetActivityTitle;
 
     public NativePageObject(AppiumDriver appiumDriver) {
         PageFactory.initElements(new AppiumFieldDecorator(appiumDriver), this);
     }
 }
+
